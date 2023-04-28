a676f25a94f02365dddc7981eb337fb75968948e
rminoro <rminoro@outlook.com>
2023-04-28 20:50:48 -0300

    Var-let-const

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+// //console.log("Olá mundo");
+
+// var nr1 = "10";
+// var nr2 = "5";
+
+// //realizando operações com Strings:
+// console.log(parseInt(nr1) + parseInt(nr2));
+
+var nome1 = "Dubas";
+// let nome2 = "Serbato";
+// const nome3="Julia";
+
+if(nome1 != ""){
+    let nome1="Julia"; //hoisting
+}
+console.log(nome1);
+
+
+
